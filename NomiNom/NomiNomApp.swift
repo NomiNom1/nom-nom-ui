374a07deea8f7b067391e07dca9e5f9ce1eca6fa
olmurphy <owen261@icloud.com>
2025-05-07 14:37:56 -0700

    added xcode files

---
+++ NomiNom/NomiNomApp.swift
@@ -0,0 +1,17 @@
+//
+//  NomiNomApp.swift
+//  NomiNom
+//
+//  Created by Owen Murphy on 5/7/25.
+//
+
+import SwiftUI
+
+@main
+struct NomiNomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NomiNomTests/NomiNomTests.swift
@@ -0,0 +1,16 @@
+//
+//  NomiNomTests.swift
+//  NomiNomTests
+//
+//  Created by Owen Murphy on 5/7/25.
+//
+
+import Testing
+
+struct NomiNomTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
