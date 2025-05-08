478acdcd84e2b843e88df915ad9d901b42415069
olmurphy <owen261@icloud.com>
2025-05-08 11:56:26 -0700

    updated the location service

--- NomiNom/Core/Views/LocationSelectorView.swift
+++ NomiNom/Core/Views/LocationSelectorView.swift
@@ -9,12 +9,7 @@ struct LocationSelectorView: View {
     var body: some View {
         NavigationView {
             VStack(spacing: 0) {
-                // Handle bar for dragging
-                RoundedRectangle(cornerRadius: 2.5)
-                    .fill(Color.gray.opacity(0.3))
-                    .frame(width: 40, height: 5)
-                    .padding(.top, 8)
-                    .padding(.bottom, 16)
+
                 
                 // Search bar
                 HStack {
