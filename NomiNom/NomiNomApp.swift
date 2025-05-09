bc4febbcc6fcf7543477228c4053dad5c60e891b
olmurphy <owen261@icloud.com>
2025-05-09 07:19:10 -0700

    removed

--- NomiNom/NomiNomApp.swift
+++ NomiNom/NomiNomApp.swift
@@ -13,7 +13,7 @@ struct NomiNomApp: App {
     
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LandingPage()
                 .environmentObject(themeManager)
                 .withTheme()
         }
